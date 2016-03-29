19e8d4bcc316922a95e6272fa8c3a6a45e012030
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2016-03-29 15:22:05 -0400

    Initial Commit

---
+++ LiveGraph/LiveGraph.h
@@ -0,0 +1,19 @@
+//
+//  LiveGraph.h
+//  LiveGraph
+//
+//  Created by Chris Rittersdorf on 3/29/16.
+//  Copyright © 2016 Collective Idea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LiveGraph.
+FOUNDATION_EXPORT double LiveGraphVersionNumber;
+
+//! Project version string for LiveGraph.
+FOUNDATION_EXPORT const unsigned char LiveGraphVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiveGraph/PublicHeader.h>
+
+
